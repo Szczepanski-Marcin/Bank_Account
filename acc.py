c9e307fb5ce0a6541ebf452b4b0ad7561fc8b424
Szczepanski-Marcin <szmar@vp.pl>
2020-07-31 21:40:13 +0200

    Added new classes

--- acc.py
+++ acc.py
@@ -1,7 +1,7 @@
 class Account:
 
     def __init__(self, filepath):
-        self.filepath=filepath
+        self.filepath = filepath
         with open(filepath, 'r') as file:
             self.balance=int(file.read())
 
@@ -15,8 +15,24 @@ class Account:
         with open(self.filepath, 'w') as file:
             file.write(str(self.balance))
 
+class Checking(Account):
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee=fee
+
+    def transfer(self, amount):
+        self.balance=self.balance - amount - self.fee
+
+checking=Checking("balance.txt", 1)
+checking.transfer(100)
+print(checking.balance)
+checking.commit()
+
+"""
 account=Account("balance.txt")
 print(account.balance)
 account.deposit(200)
 print(account.balance)
 account.commit()
+"""
